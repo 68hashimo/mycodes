511ad1a5d79888be4f60e7922758b89d152da66c
hashimo <hkdp01hashi@gmail,com>
2022-10-20 00:04:27 +0900

    re

--- getcookies.js
+++ getcookies.js
@@ -1,17 +1,12 @@
-function date(){
-    var expire = new Date();
-    expire.setTime( expire.getTime() + 1000 * 3600 * 24 );
-    //document.cookie = 'data=123; expires=' + expire.toUTCString();
-}
-
 //クッキーを連想配列として取得
 //クッキーの名前をキーとした連想配列で、すべてのクッキーを得られます。
 
 function sendcookies(){
-    const cki=document.cookie;
-    const txt=document.getElementById("txt");
-    date();
-    document.cookie ='data=123;'
+    var expire = new Date();
+    expire.setTime( expire.getTime() + 1000 * 3600 * 24 );
+    const txt=document.getElementById("txt").value;
+    document.cookie ='name='+txt+'; expires=' + expire.toUTCString();
+    document.cookie = 'data=123; max-age=5';
 }
 
 function GetCookies()
@@ -28,9 +23,31 @@ function GetCookies()
     return list;
 }
 
+
+function wrt(){
+    const tend=GetCookies()
+    //const tend=document.getElementById("txt").value;
+    var s=document.getElementById("cok");
+    s.innerHTML=tend;
+}
+
+//
+function notif(){
+    Push.create('hello',{
+      body:'test message',
+      onClick:function(){
+        //document.getElementById("c").innerHTML="bye";
+        wrt();
+        this.close()
+          
+      }  
+    });
+}
+/*
 function write(){
     let gcok= GetCookies();
     let w = document.getElementById("cok");
     w.insertAdjacentHTML('afterend', '<p>' + document.cookie + '</p>');
     //w.innerHTML(cok[0]);
 }
+*/
