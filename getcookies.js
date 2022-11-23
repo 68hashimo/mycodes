1b3b8eabe359e74b9f1f36597e93e8a4782b8c75
hashimo <hkdp01hashi@gmail,com>
2022-11-23 21:55:13 +0900

    k

--- getcookies.js
+++ getcookies.js
@@ -1,14 +1,100 @@
 //クッキーを連想配列として取得
 //クッキーの名前をキーとした連想配列で、すべてのクッキーを得られます。
+//keyを検索してそれに対応したものを消せるようにする＿これでは順が変わったときに事故る---済
+
+var ls_num = []; 
+var div=document.getElementById("div");
 
 function sendcookies(){
     var expire = new Date();
-    expire.setTime( expire.getTime() + 1000 * 3600 * 24 );
+    var flg=true;
+    expire.setTime( expire.getTime() + 1000 * 3600 * 24*365 );
+    const txt_id=document.getElementById("txt");
     const txt=document.getElementById("txt").value;
-    document.cookie ='name='+txt+'; expires=' + expire.toUTCString();
-    document.cookie = 'data=123; max-age=5';
+    console.log(txt);
+    var lst=GetCookies();
+    var lst_len=lst.length;
+    var txt_sp=txt.split(',');
+    console.log(txt_sp);
+    if(document.cookie==""){
+        if(txt_sp.length>1 && dist(lst,txt_sp,flg) && txt_er(txt,txt_sp)){
+            var i = 0;
+            for(var n=lst_len;n<(lst_len+txt_sp.length);n++){
+                document.cookie ='name'+n+'='+txt_sp[i]+'; expires=' + expire.toUTCString();
+                i++;
+            }
+        }
+        else if(txt_sp.length==1 && dist(lst,txt_sp,flg) && txt_er(txt,txt_sp)){
+            if(document.cookie==""){
+                document.cookie ='name0='+txt+'; expires=' + expire.toUTCString(); 
+            }
+            else if(lst_len==1){
+                document.cookie ='name1='+txt+'; expires=' + expire.toUTCString(); 
+            }
+            else{
+            document.cookie ='name'+(lst_len+1)+'='+txt+'; expires=' + expire.toUTCString();    
+            }
+        }
+        else{
+            console.log("no value or Duplicate");
+            //document.cookie = 'data=123; max-age=10';
+            //return;
+        }
+
+    }
+    else{
+        if(txt_sp.length>1 && dist(lst,txt_sp,flg) && txt_er(txt,txt_sp)){
+            var i = 0;
+            var l = spl()+1;
+            for(var f=lst_len;f<(lst_len+txt_sp.length);f++){
+                document.cookie ='name'+l+'='+txt_sp[i]+'; expires=' + expire.toUTCString();
+                i++;
+                l++;
+            }
+        }
+        else if(txt_sp.length==1 && dist(lst,txt_sp,flg) && txt_er(txt,txt_sp)){
+            if(document.cookie==""){
+                document.cookie ='name0='+txt+'; expires=' + expire.toUTCString(); 
+            }
+            else if(lst_len==1 && spl()!=1){
+                document.cookie ='name1='+txt+'; expires=' + expire.toUTCString(); 
+            }
+            else{
+            document.cookie ='name'+(spl()+1)+'='+txt+'; expires=' + expire.toUTCString();    
+            }
+            console.log("length=1")
+        }
+        else{
+            console.log("no value or Duplicate");
+            //document.cookie = 'data=123; max-age=10';
+            //return;
+        }
+
+    }
+    txt_id.value = '';
+    load();
+    console.log("txt_reset")
 }
 
+//重複判定
+function dist(lst_d,txt_d,flg){
+    for(var s=0;s<txt_d.length;s++){
+        for(var t =0;t<lst_d.length;t++){
+            if(txt_d[s]==lst_d[t]){
+                console.log("value exist");
+                alert("Duplicate")
+                flg=false;
+                break;
+            }
+        }
+        if(! flg){
+            break;
+        }
+    }
+    return flg;
+}
+
+//cookie取得
 function GetCookies()
 {
     let list=[];
@@ -16,38 +102,142 @@ function GetCookies()
     r.forEach(function(value) { 
     //cookie名と値に分ける
         var content = value.split('=');    
-        console.log( content[1] );
+        //console.log( content[1] );
         list.push( content[1] );
     })
-    console.log(list);
+    //console.log(list);
     return list;
 }
 
 
+//登録されているワードを返します。
 function wrt(){
-    const tend=GetCookies()
-    //const tend=document.getElementById("txt").value;
+    const gcok=GetCookies()
     var s=document.getElementById("cok");
-    s.innerHTML=tend;
+    var chk_cok = document.cookie; 
+    s.innerHTML="";
+    //s.appendChild(br);
+    if(chk_cok !=""){
+        for(var w = 0;w<gcok.length;w++){
+            var idnum = 'idnum'+w;
+            var br = document.createElement('br');
+            var checkbox = document.createElement('input');
+            checkbox.type='checkbox';
+            checkbox.className = 'checks';
+            checkbox.id = idnum;
+            checkbox.value=gcok[w];
+            var label = document.createElement('label')
+            label.htmlFor = idnum;
+            label.textContent = gcok[w];
+
+            s.appendChild(checkbox);
+            s.appendChild(label);
+            s.appendChild(br);
+        }
+    /*
+    var ck = document.cookie;
+    s.innerHTML = ck;
+    */
+    }else{
+        s.innerHTML="登録されている語句はありません"
+    }
 }
 
-//
+//削除
+
+function del_c(check_ed){
+    //const del_id=document.getElementById("del");
+    //const del=document.getElementById("del").value;
+    //const del=document.getElementById("del").value.split(",");
+    //var del_flg=true;
+    var del = check_ed;
+    const lst_del=GetCookies();
+    const del_sp=document.cookie.split(";")
+    for(var u =0;u<lst_del.length;u++){
+        if(lst_del[u]==del){
+            var c_key = del_sp[u].split("=");
+            document.cookie = c_key[0]+'='+del+'; max-age=1;'
+            var del_keynum = c_key[0].replace("name","");
+            var del_keynum=Number(del_keynum);
+            var index = ls_num.indexOf(del_keynum);
+            ls_num.splice(index,1);
+            return;
+        }
+    }
+    //alert("this value isnt existing");
+}
+
+//通知
 function notif(){
     Push.create('hello',{
       body:'test message',
       onClick:function(){
         //document.getElementById("c").innerHTML="bye";
-        wrt();
-        this.close()
-          
+        load();
+        this.close() 
       }  
     });
 }
-/*
-function write(){
-    let gcok= GetCookies();
-    let w = document.getElementById("cok");
-    w.insertAdjacentHTML('afterend', '<p>' + document.cookie + '</p>');
-    //w.innerHTML(cok[0]);
+
+//エラーワード
+function txt_er(txt_e,txt_esp){
+    if(txt_e==''){
+        alert("no value");
+        return false;
+    }
+    for(var er of txt_esp){
+        if(er==''){
+            alert('input error');
+            return false;
+        }
+    }
+    return true;
 }
-*/
+
+//名前の後についている数値の最大値を返します。
+function spl(){
+    //const lst_spl=GetCookies();
+    const spl_sp=document.cookie.split(";");
+    //var sp1 =spl_sp[spl_sp.length-1].split("=");
+    for(var sp_cok of spl_sp){
+        var sp_name =sp_cok.split("=");
+        var sp_num = sp_name[0].replace("name","");
+        sp_num=Number(sp_num);
+        ls_num.push(sp_num);
+    }
+    var vlmax = Math.max(...ls_num);
+    console.log(vlmax);
+    //div.innerHTML=ls_num;
+    ls_num=[];
+    return vlmax;
+}
+
+//チェックボックス取得
+function getchecks(){
+    var check = document.getElementsByClassName("checks");
+    del_flg = false;
+    for(var e0=0;e0<check.length;e0++){
+        if(check[e0].checked){
+            var check_ed =check[e0].value;
+            //console.log(check_ed);
+            del_c(check_ed);
+            del_flg=true;
+        }
+    }
+    if(del_flg){
+        alert("deleted")
+    }else{
+        alert("not selected")
+    }
+    load();
+}
+
+
+//onload func
+function load(){
+    wrt()
+    var load_cok=GetCookies();
+    document.getElementById('cok1').innerHTML=load_cok;
+}
+
+window.addEventListener('load',load)
