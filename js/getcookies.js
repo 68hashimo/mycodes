d76f3c84342d2fc00e22482f9e466c60f4451942
hashimo <hkdp01hashi@gmail,com>
2023-05-23 22:23:50 +0900

    cm

--- js/getcookies.js
+++ js/getcookies.js
@@ -16,7 +16,7 @@ function sendcookies(){
         return
     }
     var lst=GetCookies();
-    //svar lst_len=lst.length;
+    //var lst_len=lst.length;
     var txt_sp=txt.split(',');
     console.log(txt_sp);
     var cok_num=spl()+1;
