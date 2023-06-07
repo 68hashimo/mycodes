384412e6f80e67f6549c4ec3e7e695aaa3bcf255
hashimo <hkdp01hashi@gmail,com>
2023-06-07 18:58:47 +0900

    ds

--- js/mjs.js
+++ js/mjs.js
@@ -57,9 +57,7 @@ function userm(){
 window.onload = function(){
     var data = location.href.split("=")[1];
     var text = data.split("&")[0];
-    
-    document.getElementById("txt_cok").innerHTML = `your name:${decodeURI(text)}`;
-    
+    document.getElementById("txt_cok").innerHTML = `your name:${decodeURI(text)}`;   
     /*decodeURIComponent(text);
     var data1 = location.href.split("&")[1];
     var text1=data1.split("=")[1];
@@ -73,7 +71,33 @@ function Cookies()
 {
     //ev.click();
     console.log(GCookies());
-} 
+}
 
+//jquery_menu
+document.addEventListener('DOMContentLoaded', function(){
+  
+    const menuButton = document.getElementById('menu-button');
+    const navMenu = document.getElementById('nav-menu');
+    const overlay = document.getElementById('overlay');
+  
+    function menuToggle() {
+  
+      menuButton.classList.toggle('button-change');
+      if(menuButton.textContent == 'メニューを開く'){
+        menuButton.textContent = 'メニューを閉じる'
+      }else{
+        menuButton.textContent = 'メニューを開く'
+      }
+      
+      navMenu.classList.toggle('nav-menu-open');
+      overlay.classList.toggle('overlay-on');
+    }
+  
+    const menuEvent = document.getElementsByClassName('menu-event');
+    for(let i = 0; i < menuEvent.length; i++) {
+        menuEvent[i].addEventListener('click', menuToggle, false);
+    }
+  
+  }, false);
 // ev.style.visibility="hidden"
 ev.addEventListener('click',Cookies);
